5c5683047d3ef0b18b679ac3e54d95659c341ccc
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-05-28 17:43:38 -0500

    added contact page and test for contact page Rails

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
+    get static_pages_home_url
+    assert_response :success
   end
 
   test "should get home" do
@@ -21,4 +21,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
   end
+
+  test "should get contact" do
+     get static_pages_contact_url
+     assert_response :success
+   end
 end
