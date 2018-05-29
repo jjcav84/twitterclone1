d0ec3f179992fdcefc4e29565747a2bed9af12e6
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-05-29 09:01:29 -0500

    Remembering the user.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
@@ -14,7 +21,7 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
-  
+
   # Logs out the current user.
   def log_out
     session.delete(:user_id)
