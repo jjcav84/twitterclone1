734cc819506608185a5abb8e564891039884cbd6
Jacob Cavazos <jacobcavazos@Jacobs-MacBook-Air.local>
2018-05-29 01:20:56 -0500

    Fixed bug 2 in login test.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,6 +6,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:jacob)
   end
 
+  test "login with invalid information" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email: "", password: "" } }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with valid information" do
     get login_path
     post login_path, params: { session: { email:    @user.email,
